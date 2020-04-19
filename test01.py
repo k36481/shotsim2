68263814cec5b45bc047ba552edb8a4a8be95e75
k36481 <k36481@gmail.com>
2020-04-19 23:21:06 +0900

    test

--- test01.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def distance(p0, p1):
-    return math.sqrt((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2)
-
-for i in range(10,100):
-    for j in range(10, 100):
-        if distance((i,j),(45,45))<30:
-            print(i,j)
